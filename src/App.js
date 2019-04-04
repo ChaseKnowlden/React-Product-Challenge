df3ee97f8d9f49951a004d24b9b653adab8df0a8
Anna Pawl <annampawl@gmail.com>
2019-04-03 17:35:45 -0700

    Product Challenge

--- src/App.js
+++ src/App.js
@@ -1,22 +1,29 @@
-import React, { Component } from 'react';
-import inventory, { categories } from './inventory'
-import './App.css';
+import React, { Component } from "react";
+import inventory, { categories } from "./inventory";
+import "./App.css";
+import Product from "./Product";
 
 class App extends Component {
-
   render() {
+    const cat = categories.map(cat => {
+      return <button key={cat}>{cat} </button>;
+    });
+
+    const item = inventory.map(item => {
+      return (
+        <Product
+          key={1}
+          title={item.name}
+          desc={item.description}
+          price={item.price}
+        />
+      );
+    });
+
     return (
-      <div className="App">
-        <h1>Show products here</h1>
-
-        <ul>
-          {/* List product categories here */}
-        </ul>
-
-        <ul>
-          {/* Products listed here */}
-        </ul>
-
+      <div className="inventory">
+        <div>{cat} </div>
+        <div className="itemContainer">{item}</div>
       </div>
     );
   }
